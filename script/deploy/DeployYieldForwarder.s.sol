2339eaeb7d0b450b3bb8b7e14968a918117c37f2
CarsonCase <carson@paxoslabs.com>
2026-03-04 17:50:46 -0500

    feat: deploy scripts draft

--- script/deploy/DeployYieldForwarder.s.sol
+++ script/deploy/DeployYieldForwarder.s.sol
@@ -11,15 +11,15 @@ import "src/helper/Constants.sol";
 
 contract DeployYieldForwarder is BaseScript {
 
-    string constant NAME = "USDG0Orchestration";
-    string constant SYMBOL = "ORCH0";
+    string constant NAME = "USDG0ReverseOrchestration";
+    string constant SYMBOL = "rORCH0";
     address constant BALANCER_VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
-    address constant STRATEGIST_ADDRESS = 0x000054f89dCC1248716804E7eF5c5E225FE3a000;
+    address constant STRATEGIST_ADDRESS = 0x000054f89dCC1248716804E7eF5c5E225FE3a000; // Do we need a new address here?
     uint8 constant DECIMALS = 6;
 
-    bytes32 SALT_ROLES_AUTHORITY = 0x1Ab5a40491925cB445fd59e607330046bEac68E500677821112232323232cafe;
-    bytes32 SALT_BORING_VAULT = 0x1Ab5a40491925cB445fd59e607330046bEac68E500cafe5555555552323332fe;
-    bytes32 SALT_MANAGER_WITH_MERKLE_VERIFICATION = 0x1Ab5a40491925cB445fd59e607330046bEac68E5008828383822102320930203;
+    bytes32 SALT_ROLES_AUTHORITY = 0x1Ab5a40491925cB445fd59e607330046bEac68E500677821112232323232caff;
+    bytes32 SALT_BORING_VAULT = 0x1Ab5a40491925cB445fd59e607330046bEac68E500cafe5555555552323332ff;
+    bytes32 SALT_MANAGER_WITH_MERKLE_VERIFICATION = 0x1Ab5a40491925cB445fd59e607330046bEac68E500882838382210232093020f;
 
     function run() public broadcast {
         // deploy a roles authority
@@ -80,6 +80,10 @@ contract DeployYieldForwarder is BaseScript {
         rolesAuthority.transferOwnership(getMultisig());
         boringVault.transferOwnership(getMultisig());
         managerWithMerkleVerification.transferOwnership(getMultisig());
+
+        console.log("Yield Forwarder vault deployed at: ", address(boringVault));
+        console.log("Roles Authority deployed at: ", address(rolesAuthority));
+        console.log("Manager With Merkle Verification deployed at: ", address(managerWithMerkleVerification));
     }
 
 }
