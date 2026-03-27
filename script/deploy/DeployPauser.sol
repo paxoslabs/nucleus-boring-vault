23edcbce05df3fd045e0c904930920cd686e5c77
CarsonCase <carson@paxoslabs.com>
2026-03-27 19:09:28 -0400

    feat: pauser deploy script done correctly

--- script/deploy/DeployPauser.sol
+++ script/deploy/DeployPauser.sol
@@ -11,22 +11,21 @@ contract DeployPauser is BaseScript {
     // State variables for deployed contracts
     Pauser internal pauser;
 
-    //0xe5CcB29Cb9C886da329098A184302E2D5Ff0cD9E
-    bytes32 constant salt = 0xe5CcB29Cb9C886da329098A184302E2D5Ff0cD9Eff8888845454545454545455;
-    address constant admin = 0x6d0C5a20ac08ED00256aD224F74Ca53afF3D011d;
+    address constant EXPECTED = 0x07F99eF00C778d5D30e86C2EDB15E69e8763D5f7;
 
     function run() external broadcast {
-        address[] memory approvedPausers = new address[](2);
+        bytes32 salt = makeSalt(broadcaster, false, ":Pauser");
+        address admin = getMultisig();
+        address[] memory approvedPausers = new address[](1);
 
         approvedPausers[0] = 0xe5CcB29Cb9C886da329098A184302E2D5Ff0cD9E;
-        approvedPausers[1] = 0x12341eD9cb38Ae1b15016c6eD9F88e247f2AF76f;
 
         bytes memory creationCode = type(Pauser).creationCode;
         pauser = Pauser(CREATEX.deployCreate3(salt, abi.encodePacked(creationCode, abi.encode(admin, approvedPausers))));
+        require(address(pauser) == EXPECTED, "pauser salt does not resolve to expected address - check deployer");
         require(pauser.owner() == admin);
         require(pauser.isApprovedPauser(approvedPausers[0]));
-        require(pauser.isApprovedPauser(approvedPausers[1]));
-        console.log(address(pauser));
+        console.log("Pauser Address: ", address(pauser));
     }
 
 }
