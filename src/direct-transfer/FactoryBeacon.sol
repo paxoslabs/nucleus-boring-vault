493305b16f3b2f9c7fc9e0a4c1834b6b9a89fd68
whoislewys <whoislewys@gmail.com>
2026-04-29 17:31:51 -0400

    fix: clearer upgradeTo naming

--- src/direct-transfer/FactoryBeacon.sol
+++ src/direct-transfer/FactoryBeacon.sol
@@ -68,15 +68,15 @@ contract FactoryBeacon is UpgradeableBeacon {
      * @param newImplementation The proposed new DirectTransferAddress implementation.
      */
     function upgradeTo(address newImplementation) public override onlyOwner {
-        DirectTransferAddress currentImpl = DirectTransferAddress(implementation());
-        address currentBoringVault = currentImpl.DCD().boringVault();
-        address currentToken = address(currentImpl.token());
+        DirectTransferAddress oldImpl = DirectTransferAddress(implementation());
+        address currentBoringVault = oldImpl.DCD().boringVault();
+        address currentToken = address(oldImpl.token());
 
         super.upgradeTo(newImplementation);
 
-        DirectTransferAddress nextImpl = DirectTransferAddress(implementation());
-        address nextBoringVault = nextImpl.DCD().boringVault();
-        address nextToken = address(nextImpl.token());
+        DirectTransferAddress newImpl = DirectTransferAddress(implementation());
+        address nextBoringVault = newImpl.DCD().boringVault();
+        address nextToken = address(newImpl.token());
 
         if (nextBoringVault != currentBoringVault) revert BoringVaultMismatch(currentBoringVault, nextBoringVault);
         if (nextToken != currentToken) revert TokenMismatch(currentToken, nextToken);
