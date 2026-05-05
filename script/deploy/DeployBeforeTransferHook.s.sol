647b73ba5fb23da909d5e249eebac46d1b407300
CarsonCase <carson@paxoslabs.com>
2026-05-05 16:46:04 -0400

    fix: setting owner to multisig

--- script/deploy/DeployBeforeTransferHook.s.sol
+++ script/deploy/DeployBeforeTransferHook.s.sol
@@ -14,9 +14,9 @@ contract DeployBeforeTransferHook is BaseScript {
         bytes32 salt = 0x1ab5a40491925cb445fd59e607330046beac68e500b765d39dbd6371a4f596f9;
 
         bytes memory creationCode = type(FreezeListBeforeTransferHook).creationCode;
-        address hook = CREATEX.deployCreate3(salt, abi.encodePacked(creationCode, abi.encode(broadcaster)));
+        address hook = CREATEX.deployCreate3(salt, abi.encodePacked(creationCode, abi.encode(getMultisig())));
 
-        require(FreezeListBeforeTransferHook(hook).owner() == broadcaster, "owner mismatch");
+        require(FreezeListBeforeTransferHook(hook).owner() == getMultisig(), "owner mismatch");
 
         console.log("FreezeListBeforeTransferHook: ", hook);
         return hook;
