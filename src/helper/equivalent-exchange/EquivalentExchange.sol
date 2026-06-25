65ec1e0af6e0d90d4443b184af454c81d160e898
CarsonCase <carson@paxoslabs.com>
2026-06-25 16:08:47 -0400

    feat: auth and parameter fo subsidy provider

--- src/helper/equivalent-exchange/EquivalentExchange.sol
+++ src/helper/equivalent-exchange/EquivalentExchange.sol
@@ -3,9 +3,10 @@ pragma solidity 0.8.21;
 
 import { ERC20 } from "@solmate/tokens/ERC20.sol";
 import { SafeTransferLib } from "@solmate/utils/SafeTransferLib.sol";
+import { Auth, Authority } from "@solmate/auth/Auth.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
-contract EquivalentExchange {
+contract EquivalentExchange is Auth {
 
     using SafeTransferLib for ERC20;
     using Address for address;
@@ -18,24 +19,49 @@ contract EquivalentExchange {
     error DanglingApproval(address token);
     error InsufficientReturn(uint256 totalIn, uint256 totalOut);
 
+    constructor(address _owner, Authority _authority) Auth(_owner, _authority) { }
+
     function execute(
         ERC20[] calldata tokens,
         uint256[] calldata amountsIn,
         address[] calldata targets,
-        bytes[] calldata targetData
+        bytes[] calldata targetData,
+        address subsidyProvider,
+        ERC20 subsidyToken
     )
         external
+        requiresAuth
     {
-        uint256 tokensLength = tokens.length;
-        if (tokensLength != amountsIn.length) revert LengthMismatch();
+        if (tokens.length != amountsIn.length) revert LengthMismatch();
+        if (targets.length != targetData.length) revert LengthMismatch();
 
-        uint256 targetsLength = targets.length;
-        if (targetsLength != targetData.length) revert LengthMismatch();
+        (uint8[] memory tokenDecimals, uint256 totalIn) = _pull(tokens, amountsIn);
 
-        uint8[] memory tokenDecimals = new uint8[](tokensLength);
-        uint256 totalIn;
+        for (uint256 i; i < targets.length; ++i) {
+            targets[i].functionCall(targetData[i]);
+        }
 
-        for (uint256 i; i < tokensLength; ++i) {
+        uint256 totalOut = _sweep(tokens, tokenDecimals);
+
+        if (totalOut < totalIn) {
+            totalOut += _coverShortfall(subsidyToken, subsidyProvider, totalIn - totalOut);
+        }
+
+        if (totalOut < totalIn) revert InsufficientReturn(totalIn, totalOut);
+
+        emit Executed(msg.sender, totalIn, totalOut);
+    }
+
+    function _pull(
+        ERC20[] calldata tokens,
+        uint256[] calldata amountsIn
+    )
+        internal
+        returns (uint8[] memory tokenDecimals, uint256 totalIn)
+    {
+        tokenDecimals = new uint8[](tokens.length);
+
+        for (uint256 i; i < tokens.length; ++i) {
             ERC20 token = tokens[i];
             uint8 decimals = token.decimals();
             tokenDecimals[i] = decimals;
@@ -50,23 +76,29 @@ contract EquivalentExchange {
                 revert DanglingApproval(address(token));
             }
         }
+    }
 
-        for (uint256 i; i < targetsLength; ++i) {
-            targets[i].functionCall(targetData[i]);
-        }
-
-        uint256 totalOut;
-
-        for (uint256 i; i < tokensLength; ++i) {
+    function _sweep(ERC20[] calldata tokens, uint8[] memory tokenDecimals) internal returns (uint256 totalOut) {
+        for (uint256 i; i < tokens.length; ++i) {
             ERC20 token = tokens[i];
             uint256 balance = token.balanceOf(address(this));
             if (balance > 0) token.safeTransfer(msg.sender, balance);
             totalOut += _normalize(balance, tokenDecimals[i]);
         }
+    }
 
-        if (totalOut < totalIn) revert InsufficientReturn(totalIn, totalOut);
-
-        emit Executed(msg.sender, totalIn, totalOut);
+    function _coverShortfall(
+        ERC20 subsidyToken,
+        address subsidyProvider,
+        uint256 shortfall
+    )
+        internal
+        returns (uint256)
+    {
+        uint8 subsidyDecimals = subsidyToken.decimals();
+        uint256 subsidyAmount = _denormalize(shortfall, subsidyDecimals);
+        subsidyToken.safeTransferFrom(subsidyProvider, msg.sender, subsidyAmount);
+        return _normalize(subsidyAmount, subsidyDecimals);
     }
 
     function _normalize(uint256 amount, uint8 decimals) internal pure returns (uint256) {
@@ -74,6 +106,14 @@ contract EquivalentExchange {
             return amount * (10 ** (NORMALIZED_DECIMALS - decimals));
         }
         return amount / (10 ** (decimals - NORMALIZED_DECIMALS));
+    }
+
+    function _denormalize(uint256 normalizedAmount, uint8 decimals) internal pure returns (uint256) {
+        if (decimals <= NORMALIZED_DECIMALS) {
+            uint256 factor = 10 ** (NORMALIZED_DECIMALS - decimals);
+            return (normalizedAmount + factor - 1) / factor;
+        }
+        return normalizedAmount * (10 ** (decimals - NORMALIZED_DECIMALS));
     }
 
 }
