ca770ddc45956a8b021ba33ad7d0005a8db32fc7
Nick Barry <itsnickbarry@protonmail.ch>
2026-06-26 22:18:44 +0900

    test: test dangling approval revert for unused token (0 amount in)

--- test/helper/equivalent-exchange/EquivalentExchange.t.sol
+++ test/helper/equivalent-exchange/EquivalentExchange.t.sol
@@ -341,4 +341,24 @@ contract EquivalentExchangeTest is Test {
         assertEq(tokenB.balanceOf(address(exchange)), 0, "exchange should have no tokenB");
     }
 
+    function test_Execute_RevertsIfDanglingApprovalOnZeroAmountToken() external {
+        tERC20 token = new tERC20(18);
+        deal(address(token), owner, 1e18);
+
+        ERC20[] memory tokens = new ERC20[](1);
+        uint256[] memory amountsIn = new uint256[](1);
+        address[] memory targets = new address[](0);
+        bytes[] memory targetData = new bytes[](0);
+
+        tokens[0] = ERC20(address(token));
+        amountsIn[0] = 0;
+
+        vm.startPrank(owner);
+        token.approve(address(exchange), 1e18);
+
+        vm.expectRevert(abi.encodeWithSelector(EquivalentExchange.DanglingApproval.selector, address(token)));
+        exchange.execute(tokens, amountsIn, targets, targetData, address(0), ERC20(address(0)));
+        vm.stopPrank();
+    }
+
 }
