f68d90d5790252b535f111a012c7887707fdf6e8
Nick Barry <itsnickbarry@protonmail.ch>
2026-06-26 22:02:33 +0900

    test: test dangling approvals and subsidy payment revert cases

--- test/helper/equivalent-exchange/EquivalentExchange.t.sol
+++ test/helper/equivalent-exchange/EquivalentExchange.t.sol
@@ -16,6 +16,12 @@ contract RevertingTarget {
 
 }
 
+contract tERC20 is ERC20 {
+
+    constructor(uint8 _decimals) ERC20("test token", "TT", _decimals) { }
+
+}
+
 contract EquivalentExchangeTest is Test {
 
     EquivalentExchange internal exchange;
@@ -88,6 +94,78 @@ contract EquivalentExchangeTest is Test {
         vm.prank(owner);
         vm.expectRevert("UNAUTHORIZED");
         exchange.execute(tokens, amountsIn, targets, targetData, address(0), ERC20(address(0)));
+    }
+
+    function test_Execute_RevertsIfDanglingApproval() external {
+        tERC20 token = new tERC20(18);
+        deal(address(token), owner, 2e18);
+
+        ERC20[] memory tokens = new ERC20[](1);
+        uint256[] memory amountsIn = new uint256[](1);
+        address[] memory targets = new address[](0);
+        bytes[] memory targetData = new bytes[](0);
+
+        tokens[0] = ERC20(address(token));
+        amountsIn[0] = 1e18;
+
+        vm.startPrank(owner);
+        token.approve(address(exchange), 2e18);
+
+        vm.expectRevert(abi.encodeWithSelector(EquivalentExchange.DanglingApproval.selector, address(token)));
+        exchange.execute(tokens, amountsIn, targets, targetData, address(0), ERC20(address(0)));
+        vm.stopPrank();
+    }
+
+    function test_Execute_RevertsIfSubsidyProviderHasInsufficientBalance() external {
+        address subsidyProvider = makeAddr("subsidyProvider");
+        address recipient = makeAddr("recipient");
+        tERC20 token = new tERC20(18);
+        deal(address(token), owner, 1e18);
+
+        ERC20[] memory tokens = new ERC20[](1);
+        uint256[] memory amountsIn = new uint256[](1);
+        address[] memory targets = new address[](1);
+        bytes[] memory targetData = new bytes[](1);
+
+        tokens[0] = ERC20(address(token));
+        amountsIn[0] = 1e18;
+        targets[0] = address(token);
+        targetData[0] = abi.encodeWithSelector(ERC20.transfer.selector, recipient, 1e18);
+
+        vm.prank(subsidyProvider);
+        token.approve(address(exchange), 1e18);
+
+        vm.startPrank(owner);
+        token.approve(address(exchange), 1e18);
+
+        vm.expectRevert("TRANSFER_FROM_FAILED");
+        exchange.execute(tokens, amountsIn, targets, targetData, subsidyProvider, ERC20(address(token)));
+        vm.stopPrank();
+    }
+
+    function test_Execute_RevertsIfSubsidyProviderHasInsufficientAllowance() external {
+        address subsidyProvider = makeAddr("subsidyProvider");
+        address recipient = makeAddr("recipient");
+        tERC20 token = new tERC20(18);
+        deal(address(token), owner, 1e18);
+        deal(address(token), subsidyProvider, 1e18);
+
+        ERC20[] memory tokens = new ERC20[](1);
+        uint256[] memory amountsIn = new uint256[](1);
+        address[] memory targets = new address[](1);
+        bytes[] memory targetData = new bytes[](1);
+
+        tokens[0] = ERC20(address(token));
+        amountsIn[0] = 1e18;
+        targets[0] = address(token);
+        targetData[0] = abi.encodeWithSelector(ERC20.transfer.selector, recipient, 1e18);
+
+        vm.startPrank(owner);
+        token.approve(address(exchange), 1e18);
+
+        vm.expectRevert("TRANSFER_FROM_FAILED");
+        exchange.execute(tokens, amountsIn, targets, targetData, subsidyProvider, ERC20(address(token)));
+        vm.stopPrank();
     }
 
 }
