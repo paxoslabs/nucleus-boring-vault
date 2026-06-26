371101252881f11d30cd373dd89d27adb14f2ef1
CarsonCase <carson@paxoslabs.com>
2026-06-25 21:53:18 -0400

    chore: comment balancer

--- script/deploy/DeployTransitStation.s.sol
+++ script/deploy/DeployTransitStation.s.sol
@@ -36,7 +36,7 @@ contract DeployTransitStation is BaseScript {
     uint8 constant DECIMALS = 6;
 
     uint64 constant MESSAGE_GAS_LIMIT = 400_000;
-    address constant BALANCER_VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+    // address constant BALANCER_VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
 
     // ---- Route assets (offerAsset/wantAsset for setRouteApprovals) ----
     address constant USDC_ETH = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
